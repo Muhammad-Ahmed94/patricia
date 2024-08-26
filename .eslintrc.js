86e9e10e5bb1723f1e54042ebf82c321a210bafd
Muneeb Ahmed94 <muneebbum757@gmail.com>
2024-08-27 00:00:28 +0500

    eslint  rules change

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
+const { error } = require('console')
+
 module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
   plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
 }
