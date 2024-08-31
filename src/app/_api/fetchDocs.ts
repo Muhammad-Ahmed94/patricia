dec26992afa749646fee6ceabb5c508a0d392401
Muneeb Ahmed94 <muneebbum757@gmail.com>
2024-08-31 14:50:55 +0500

    Hero section complete category component created

--- src/app/_api/fetchDocs.ts
+++ src/app/_api/fetchDocs.ts
@@ -1,11 +1,14 @@
 import type { RequestCookie } from 'next/dist/compiled/@edge-runtime/cookies'
 
 import type { Config } from '../../payload/payload-types'
-import { ORDERS } from '../_graphql/orders'
-import { PAGES } from '../_graphql/pages'
-import { PRODUCTS } from '../_graphql/products'
-import { GRAPHQL_API_URL } from './shared'
-import { payloadToken } from './token'
+import { query } from 'express';
+
+import { CATEGORIES } from '../_graphql/categories';
+import { ORDERS } from '../_graphql/orders';
+import { PAGES } from '../_graphql/pages';
+import { PRODUCTS } from '../_graphql/products';
+import { GRAPHQL_API_URL } from './shared';
+import { payloadToken } from './token';
 
 const queryMap = {
   pages: {
@@ -19,6 +22,10 @@ const queryMap = {
   orders: {
     query: ORDERS,
     key: 'Orders',
+  },
+  categories: {
+    query: CATEGORIES,
+    key: 'Categories',
   },
 }
 
